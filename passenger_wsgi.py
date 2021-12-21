cea6af783a6e3b917cc50920bc8009aa34b3dfc7
darkstorn <inikishin@gmail.com>
2021-12-21 18:57:09 +0300

    fix passenger_wsgi.py

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,8 +1,6 @@
-# file for reg.ru hosting
 import os, sys
-from django.core.wsgi import get_wsgi_application
-
 sys.path.insert(0, '/var/www/u1016303/data/www/api.pandoracapitalmanagement.ru/pandora_capital_management_backend')
 sys.path.insert(1, '/var/www/u1016303/data/prenv/lib/python3.7/site-packages')
 os.environ['DJANGO_SETTINGS_MODULE'] = 'pandora_capital_management_backend.settings'
+from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
